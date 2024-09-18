5ab24aef9aa57d68d00c5fcf22fb4aef71c07165
Katerina Paparizou <76427845+Katerinajames@users.noreply.github.com>
2024-09-18 12:10:12 +0300

    Add files via upload
    We enter two numbers, and we examine if the first is a multiple of the second using if control structure

---
+++ multiple.py
@@ -0,0 +1,9 @@
+first=int(input("Enter  first integer\n"))
+
+
+second=int(input("Enter  second  integer\n"))
+
+if first%second==0:
+	print ("The number  %d is multiple of the number  %d"%(first,second))
+else:
+		print ("The number  %d is not  multiple of the number  %d"%(first,second))
